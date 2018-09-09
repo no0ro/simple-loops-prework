0d0f6450fc4f16f3d570aa40aee3fb465d90c53d
Noelle Rolwes <nrolwes@gmail.com>
2018-09-09 12:18:57 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -45,7 +45,7 @@ end
 
 def for_iterator(number_of_times)
   i = 0..number_of_times
-  for number in number_of_times
+  for num in number_of_times do
     puts phrase = "Welcome to Flatiron School's Web Development Course!"
   end
 end
